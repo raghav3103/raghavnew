27ee78cc4fd3b065d87b74451654bd045fad8b99
arora <arora@LAPTOP-0MT7PC1D>
2020-06-17 18:09:18 +0530

    new project

---
+++ com.raghav/src/main/java/com/raghav/Rag.java
@@ -0,0 +1,20 @@
+package com.raghav;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Rag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		 
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+
+		driver.manage().window().maximize();
+		
+	}
+
+}
